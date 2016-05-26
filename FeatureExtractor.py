2b25eb1fc579f7ed31e679971c5dfb5480cb4356
Gautam <gautam20197@gmail.com>
2016-05-26 14:58:30 +0530

    Remove punctuations and stop words

--- FeatureExtractor.py
+++ FeatureExtractor.py
@@ -1,12 +1,26 @@
 import nltk 
+import string
+import re
+from nltk.tokenize import RegexpTokenizer
+from nltk.corpus import stopwords
 
+# This function is used to extract the sentences from a document
 def extraction(document):
+	# punkt uses unsupervised learning to learn how to extract sentences
+	tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
 	f = open(document,'rU')
 	docUnform = f.read()
-	tokens = nltk.word_tokenize(docUnform)
+	tokens = nltk.word_tokenize(docUnform) # to remove the extra /n's
 	docForm = ' '.join(tokens)
-	sentences = docForm.split('.')
+	doc = tokenizer.tokenize(docForm) # Punctuations are still present in the sentences
+	sentences = []
+	for i in doc:
+		i = i.lower()
+		# RegexpTokenizer divides a sring into substrings based on alphabetical sequences
+		tokenizer = RegexpTokenizer(r'\w+')
+		tokens = tokenizer.tokenize(i)
+		filtered_words = [w for w in tokens if not w in stopwords.words('english')]
+		sentences.append(" ".join(filtered_words))
 	return sentences
 
-a = extraction('d061.txt')
-print(a[0])
+a = extraction('d061.txt') #This variable stores the array of sentences 
