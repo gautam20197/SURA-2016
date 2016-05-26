4a657c994387ab49a32a62aa10c2df5a8ec8aed6
Gautam <gautam20197@gmail.com>
2016-05-26 15:58:53 +0530

    Applied the tf-idf weighing system

--- FeatureExtractor.py
+++ FeatureExtractor.py
@@ -1,6 +1,7 @@
 import nltk 
 import string
 import re
+import math
 from nltk.tokenize import RegexpTokenizer
 from nltk.corpus import stopwords
 
@@ -23,4 +24,31 @@ def extraction(document):
 		sentences.append(" ".join(filtered_words))
 	return sentences
 
+# This function takes a string as an input to return an array of weights of all the terms (only index terms)
+def weight(sentence):
+	weights = []
+	words = nltk.word_tokenize(sentence)
+	freqDist = nltk.FreqDist(words)
+	(a,b) = freqDist.most_common(1)[0]
+	for i in words:
+		tf = freqDist[i]/b
+		# N has been defined below as the number of total sentences
+		isf = math.log(N/occurence(i))
+		weights.append(tf*isf)
+	return weights
+
+# This function returns the number of sentences a given string appears in
+def occurence(word):
+	count = 0
+	for sentence in a:
+		if (word in sentence):
+			count+=1
+	return count
+
 a = extraction('d061.txt') #This variable stores the array of sentences 
+N = len(a) 
+print(N)
+print(a[0])
+print(weight(a[0]))
+print(occurence('gilbert'))
+
