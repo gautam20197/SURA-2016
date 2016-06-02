78b7db9aa6dae7c263c26bfc8920ac645e2b7bf1
icygurkirat <gkiratbajwa@gmail.com>
2016-06-03 05:05:27 +0530

    Implemented Genetic Algorithm for  a fixed fitness function

--- GeneticAlgorithm.py
+++ GeneticAlgorithm.py
@@ -68,20 +68,20 @@ def reswap(child1,child2,S,N):
 def mutation(chromosome,N):
 	ind=random.randint(0,N-1)
 	if(ind==0):
-		if(chromosome[0]==1&&chromosome[1]==0):
+		if(chromosome[0]==1 and chromosome[1]==0):
 			(chromosome[0],chromosome[1])=(0,1)
 		else:
 			mutation(chromosome,N)
 	elif(ind==N-1):
-		if(chromosome[N-1]==1&&chromosome[N-2]==0):
+		if(chromosome[N-1]==1 and chromosome[N-2]==0):
 			(chromosome[N-1],chromosome[N-2])=(0,1)
 		else:
 			mutation(chromosome,N)
 	else:
 		if(chromosome[ind]==1):
-			if(chromosome[ind-1]==0&&chromosome[ind+1]==1):
+			if(chromosome[ind-1]==0 and chromosome[ind+1]==1):
 				(chromosome[ind],chromosome[ind-1])=(0,1)
-			elif(chromosome[ind-1]==1&&chromosome[ind+1]==0):
+			elif(chromosome[ind-1]==1 and chromosome[ind+1]==0):
 				(chromosome[ind],chromosome[ind+1])=(0,1)
 			else:
 				ind2=random.randint(0,1)
@@ -93,31 +93,98 @@ def mutation(chromosome,N):
 			mutation(chromosome,N)
 
 
+#function to find the two individuals with the smallest fitness values
+def smallest(fitnesses):
+	min1=0
+	min2=0
+	ind1=0
+	ind2=0
+	if(fitnesses[0]<fitnesses[1]):
+		(min1,min2,ind1,ind2)=(fitnesses[0],fitnesses[1],0,1)
+	else:
+		(min1,min2,ind1,ind2)=(fitnesses[1],fitnesses[0],1,0)
+	for i in range(2,len(fitnesses)):
+		if(fitnesses[i]<min1):
+			min2=min1
+			ind2=ind1
+			min1=fitnesses[i]
+			ind1=i
+		elif(fitnesses[i]<min2):
+			min2=fitnesses[i]
+			ind2=i
+	return [ind1,ind2]
+
 			
 
+#The overall genetic algorithm for a fixed fitness function
 def GA(size,S,N):
+	#initialize a random population of required size
 	population=initPopulation(size,S,N)
 	fitnesses=[fitness(x) for x in population]
+	#best two individuals selected
 	best=elitism(fitnesses)
 
-
-	(fitnesses[best[0]],fitnesses[best[1]])=(0,0)
-	denom=sum(fitnesses)
-	fitnesses=[x/denom for x in fitnesses]
-	select=np.random.choice(range(size),2,replace=False,p=fitnesses)
-
-
-	offspring=crossover(population[select[0]],population[select[1]],S,N)
+	for i in range(25):
+		#Phase1:- Selection of individuals for reproduction
+		denom=sum(fitnesses)-fitnesses[best[0]]-fitnesses[best[1]]
+		probabilities=[x/denom for x in fitnesses]
+		(probabilities[best[0]],probabilities[best[1]])=(0,0)
+		select=np.random.choice(range(size),2,replace=False,p=probabilities)
 
 
-	mutate=random.randint(0,1)
-	if(mutate==1):
-		mutation(offspring[0],N)
-	mutate=random.randint(0,1)
-	if(mutate==1)
-		mutation(offspring[1],N)
+		#crossover between individuals
+		offspring=crossover(population[select[0]],population[select[1]],S,N)
 
 
+		#conditional mutation of the offsprings based on probability
+		mutate=random.randint(0,1)
+		if(mutate==1):
+			mutation(offspring[0],N)
+		mutate=random.randint(0,1)
+		if(mutate==1):
+			mutation(offspring[1],N)
+		(f1,f2)=(fitness(offspring[0]),fitness(offspring[1]))
+
+		
+		#pruning the set by removing the two weakest individuals from the new set
+		worst=smallest(fitnesses)
+		temp=[f1,f2,fitnesses[worst[0]],fitnesses[worst[1]]]
+		tempBest=elitism(temp)
+		if(sum(tempBest)==1):
+			(population[worst[0]],population[worst[1]],fitnesses[worst[0]],fitnesses[worst[1]])=(offspring[0],offspring[1],f1,f2)
+		elif(sum(tempBest)==2):
+			(population[worst[1]],fitnesses[worst[1]])=(offspring[0],f1)
+		elif(sum(tempBest)==4):
+			(population[worst[0]],fitnesses[worst[0]])=(offspring[1],f2)
+		elif(sum(tempBest)==3 and (tempBest[0]==0 or tempBest[0]==3)):
+			(population[worst[0]],fitnesses[worst[0]])=(offspring[0],f1)
+		elif(sum(tempBest)==3 and (tempBest[0]==1 or tempBest[1]==2)):
+			(population[worst[1]],fitnesses[worst[1]])=(offspring[1],f2)
+
+		
+		#finding new fittest individuals 
+		temp=[fitnesses[best[0]],fitnesses[best[1]],fitnesses[worst[0]],fitnesses[worst[1]]]
+		tempBest=elitism(temp)
+
+		if(tempBest[0]==0):
+			tempBest[0]=best[0]
+		elif(tempBest[0]==1):
+			tempBest[0]=best[1]
+		elif(tempBest[0]==2):
+			tempBest[0]=worst[0]
+		elif(tempBest[0]==3):
+			tempBest[0]=worst[1]
+		if(tempBest[1]==0):
+			tempBest[1]=best[0]
+		elif(tempBest[1]==1):
+			tempBest[1]=best[1]
+		elif(tempBest[1]==2):
+			tempBest[1]=worst[0]
+		elif(tempBest[1]==3):
+			tempBest[1]=worst[1]
+		
+		(best[0],best[1])=(tempBest[0],tempBest[1])
+		print((fitnesses[best[0]],fitnesses[best[1]]))
 
 
 GA(20,(int)(N/2),N)
